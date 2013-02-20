edf3d5eacfb45b2cd182a180917829d41ec58e0e
Tamás Dévai <dorionka@freemail.hu>
2013-02-20 17:50:24 +0100

    Initial form with google geocode autocomplete.

--- src/NIIF/GN3/GHGSimulatorBundle/Controller/ConferenceController.php
+++ src/NIIF/GN3/GHGSimulatorBundle/Controller/ConferenceController.php
@@ -4,10 +4,41 @@ namespace NIIF\GN3\GHGSimulatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+use Symfony\Component\HttpFoundation\Request;
+
+use NIIF\GN3\GHGSimulatorBundle\Entity\Conference;
+
+class ConferenceController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
-        return $this->render('NIIFGN3GHGSimulatorBundle:Default:index.html.twig', array('name' => $name));
+        $conference = new Conference();
+
+        $form = $this->createFormBuilder($conference)
+          ->add(
+              'confLocation',
+              'text',
+              array(
+                'label' => 'Conference Location: ',
+                'max_length' => 35,
+              )
+            )
+            ->add('confDuration', 'text', array('label' => 'Confernce Duration: '))
+            ->add('participantLocations', 'text', array('label' => 'Participant Location: ', ))
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // perform some action, such as saving the task to the database
+
+                return $this->redirect($this->generateUrl('ghgsimulator'));
+            }
+        }
+
+        return $this->render('NIIFGN3GHGSimulatorBundle:Conference:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
